381f82406832b20a7933fc44eedf2c30b71c816c
Resn4R <purview-joust.0x@icloud.com>
2023-12-17 14:49:17 +0000

    refactored return statement

---
+++ sample.py
@@ -0,0 +1,6 @@
+def are_you_playing_banjo(name):
+    string = array(name)
+    if name[0] == 'r' || name[0] == 'R'
+        return name + " plays banjo"
+    else 
+        return name + " does not play banjo"
