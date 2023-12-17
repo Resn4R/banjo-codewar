a07cdddafbb57bc24a26d2c2b2421ca1d84a27e8
Resn4R <purview-joust.0x@icloud.com>
2023-12-17 14:50:36 +0000

    refactored condition statement

--- sample.py
+++ sample.py
@@ -1,6 +1,2 @@
 def are_you_playing_banjo(name):
-    string = array(name)
-    if name[0] == 'r' || name[0] == 'R'
-        return name + " plays banjo"
-    else 
-        return name + " does not play banjo"
+    return name + " plays banjo" if name[0] == 'r' or [0] == 'R' else name + " does not play banjo"
